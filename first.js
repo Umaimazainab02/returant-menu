c2d0bc04fd8533dd1462490f804f8703d52d4870
Umaima Zainab <umaimazainab429@gmail.com>
2025-09-18 19:34:36 +0500

    Update files

--- first.js
+++ first.js
@@ -1,6 +1,7 @@
-document.querySelectorAll('.food-card1 button').forEach(button => {
+document.querySelectorAll('.food-items-row button').forEach(button => {
     button.addEventListener('click', () => {
         alert('Item added to cart!');
     });
 
 });
+
