870333cfa1d0ce04bd6a131a67acfdec4086b661
Umaima Zainab <umaimazainab429@gmail.com>
2025-09-22 19:26:11 +0500

    Update files

--- first.js
+++ first.js
@@ -1,13 +1,16 @@
 let cartCount = 0;
 let addToCartButtons = document.querySelectorAll('.add-to-cart');
 const cartCountDisplay = document.querySelector('#cart-count');
+const cartItemsList = document.querySelector('#cart-items');
+const cartIcon = document.querySelector('.cart-icon');
+const cartBox = document.querySelector('#cart-box');
+
 addToCartButtons.forEach(button => {
     button.addEventListener('click', () => {
         cartCount++;
         if(cartCountDisplay){
         cartCountDisplay.textContent = cartCount; 
         }
-            alert('Item added to cart!');
 
 })
 });
